a9dd6f43033794a891c0f17374b15164a23cd2ab
martinvasko <martin@expressflow.com>
2021-12-14 10:34:52 +0100

    Renaming

--- part-2/adapter/adapterBase.py
+++ part-2/adapter/adapterBase.py
@@ -46,8 +46,10 @@ if __name__ == "__main__":
     print("\n")
 
     adaptee = Adaptee()
-    print("Client: The Adaptee class has a weird interface. "
-          "See, I don't understand it:")
+    print(
+        "Client: The Adaptee class has a weird interface. "
+        "See, I don't understand it:"
+    )
     print(f"Adaptee: {adaptee.specific_request()}", end="\n\n")
 
     print("Client: But I can work with it via the Adapter:")
